43e704ff73b25fead0ec82ceca613e3bf7f3816c
felipekgouvea <felipekinupesg@hotmail.com>
2024-05-06 08:40:28 -0300

    feat: add initial Search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,14 @@
 import Header from "../_components/header/page";
+import Search from "../_components/search/page";
 
 const Home = () => {
   return (
-    <div className="px-5 py-6">
-      <Header />
-    </div>
+    <>
+      <div className="flex flex-col gap-6 px-5 py-6">
+        <Header />
+        <Search />
+      </div>
+    </>
   );
 };
 
