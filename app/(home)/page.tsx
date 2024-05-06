8206784748ca3f36f073ad8cd21c9b33d6c728e6
felipekgouvea <felipekinupesg@hotmail.com>
2024-05-06 10:06:43 -0300

    feat: add promo banner to homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "../_components/category/category-list";
 import Header from "../_components/header/page";
 import Search from "../_components/search/page";
@@ -9,6 +10,15 @@ const Home = () => {
         <Header />
         <Search />
         <CategoryList />
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas."
+          width={0}
+          height={0}
+          sizes="100vh"
+          quality={100}
+          className="h-auto w-full object-contain"
+        />
       </div>
     </>
   );
