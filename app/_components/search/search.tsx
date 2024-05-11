6fa8087580d0d35f4b0908f9b915cf6021cf2f46
felipekgouvea <felipekinupesg@hotmail.com>
2024-05-11 15:35:14 -0300

    feat: ensure restaurants can be searched for

--- app/_components/search/search.tsx
+++ app/_components/search/search.tsx
@@ -1,16 +1,71 @@
-import { SearchIcon } from "lucide-react";
-import { Button } from "../ui/button";
-import { Input } from "../ui/input";
+'use client'
 
-const Search = () => {
+import { SearchIcon } from 'lucide-react'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { z } from 'zod'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '../ui/form'
+
+const createFormSchema = z.object({
+  search: z
+    .string({
+      required_error: 'Campo obrigatório',
+    })
+    .trim(),
+})
+
+interface SearchProps {
+  defaultValues?: z.infer<typeof createFormSchema>
+}
+
+type CreateFormSchema = z.infer<typeof createFormSchema>
+
+const Search = ({ defaultValues }: SearchProps) => {
+  const router = useRouter()
+
+  const form = useForm<CreateFormSchema>({
+    resolver: zodResolver(createFormSchema),
+    defaultValues,
+  })
+
+  const handleSubmit = (data: CreateFormSchema) => {
+    router.push(`/restaurants?search=${data.search}`)
+  }
+
   return (
-    <div className="flex gap-2">
-      <Input placeholder="Buscar Restaurante" className="border-none" />
-      <Button size="icon">
-        <SearchIcon size={20} />
-      </Button>
-    </div>
-  );
-};
+    <div className="flex items-center gap-2">
+      <Form {...form}>
+        <form
+          className="flex w-full gap-4"
+          onSubmit={form.handleSubmit(handleSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    className="border-none"
+                    placeholder="Busque por um restaurante"
+                    {...field}
+                  />
+                </FormControl>
 
-export default Search;
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button size="icon" type="submit">
+            <SearchIcon size={20} />
+          </Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default Search
--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -6,8 +6,15 @@ import { useEffect, useState } from 'react'
 import { Restaurant } from '@prisma/client'
 import { searchForRestaurant } from './_actions/search'
 import RestaurantItem from '../_components/restaurant/restaurant-item'
+import Search from '../_components/search/search'
 
-const Restaurants = () => {
+interface RestaurantsProps {
+  params: {
+    search?: string
+  }
+}
+
+const Restaurants = ({ params }: RestaurantsProps) => {
   const searchParams = useSearchParams()
   const [restaurants, setRestaurants] = useState<Restaurant[]>([])
 
@@ -25,18 +32,28 @@ const Restaurants = () => {
     <>
       <div className="px-5 pt-6">
         <Header />
+
+        <div className="mt-4">
+          <Search
+            defaultValues={{
+              search: params.search as any,
+            }}
+          />
+        </div>
+
         <h2 className="pb-6 pt-6 text-lg font-semibold">
           Restaurantes Encontrados
         </h2>
       </div>
       <div className="flex flex-col gap-6 px-5">
-        {restaurants.map((restaurant) => (
-          <RestaurantItem
-            restaurant={restaurant}
-            key={restaurant.id}
-            className="min-w-full max-w-full"
-          />
-        ))}
+        {restaurants &&
+          restaurants.map((restaurant) => (
+            <RestaurantItem
+              restaurant={restaurant}
+              key={restaurant.id}
+              className="min-w-full max-w-full"
+            />
+          ))}
       </div>
     </>
   )
